ef561a5e66be13da77acc42b4ce9b02d6e596e1f
Trish Wynne <trishas_one@hotmail.com>
2018-08-03 12:23:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 // write your code below!
 function happyHolidays() {
-  console.log("Happy Holidays!")
+  return "Happy Holidays!"
 }
 
 function happyHolidaysto(firstName) {
-  return (`Hello, ${firstName}!`)
+  return `Hello, ${firstName}!`
 }
 
 function happyHolidayto(holiday, firstName) {
-  return (`Happy ${holiday}, ${firstName}!`)
+  return `Happy ${holiday}, ${firstName}!`
 }
 
 function holidayCountdown(days, holiday) {
